13a3e36446445eb0261b7e5550cddb422d39f75e
Sam-Izdat <ghsamizdat@gmail.com>
2024-11-23 10:25:44 -0600

    canvas sizing issue

--- src/main.ts
+++ src/main.ts
@@ -316,6 +316,8 @@ const executeFrame = (dt: number = 0, reqWidth: number = 0, reqHeight: number = 
   // Ensure we're sized properly w.r.t. pixel ratio
   const rect = gpu.container.getBoundingClientRect();
   if (reqWidth > 0 && reqHeight > 0) {
+    gpu.dims[0] = reqWidth;
+    gpu.dims[1] = reqHeight;
     gpu.canvas.width = reqWidth;
     gpu.canvas.height = reqHeight;
     gpu.canvas.style.width = `${reqWidth}px`;
